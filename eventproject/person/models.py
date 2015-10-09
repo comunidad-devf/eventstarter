c08fe3284cbd2db7e4e2a4609be9422d78515cbd
Anuar <anuarharb@hotmail.com>
2015-10-08 22:30:47 -0500

    User Model created

--- eventproject/person/models.py
+++ eventproject/person/models.py
@@ -4,5 +4,11 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User)
+    avatar = models.URLField
+    bio = models.CharField(max_length=500)
+    birth_day = models.DateTimeField()
+    phone = models.IntegerField()
+    score = models.IntegerField()
     fb_url = models.URLField(null=True)
     twitter_url = models.URLField(null=True)
+    fraudulent_user = models.BooleanField(default=False)
