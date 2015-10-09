bdca1426b67e679d44e9229b3d04b63e4bb3eacb
Pablo Trinidad <pabloatp0204@gmail.com>
2015-10-08 20:39:22 -0500

    User Profiles Model

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+"""User Profiles model."""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+
+    """User Profile model."""
+
+    user = models.ForeignKey(User)
+    avatar = models.URLField()
+    biography = models.TextField(null=True, blank=True)
+    birthday = models.DateField()
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    score = models.IntegerField(default=0)
+    facebook_url = models.URLField(null=True, blank=True)
+    twitter_url = models.URLField(null=True, blank=True)
+    fraudulent = models.BooleanField(default=False)
