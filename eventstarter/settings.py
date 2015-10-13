45d58ac048bd9df850bcd8ff797f69ca5619f571
Mauricio Munguia <maumg1196@gmail.com>
2015-10-12 22:09:54 -0500

    adminuser

--- eventstarter/settings.py
+++ eventstarter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+SECRET_KEY = "t#%qjs91h78c#uc61tyj=g8dt$jagbeago5ei=z8jmf&$mqfyw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- userprofiles/admin.py
+++ userprofiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'score', 'created', 'fraudulent')
+    list_filter = ('score', 'fraudulent',)
+    list_display_links = ('user',)
+    search_fields = ('user',)
