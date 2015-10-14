986d517bfe1cd75d06bd4d8583d382a79a9ff574
Pablo Trinidad <pabloatp0204@gmail.com>
2015-10-13 22:16:11 -0500

    User Session with facebook

--- events/views.py
+++ events/views.py
@@ -1,4 +1,23 @@
+"""Events views."""
 from django.shortcuts import render
+from userprofiles.models import UserProfile
+
 
 def events_home(request):
+    """Home View.
+
+    This view renders the main page content and have the main
+    task of creating a User Profile entity.
+    """
+    # Check if user session has an User Profile entity.
+    if request.user.is_authenticated():
+        try:
+            user_profile = UserProfile.objects.get(user=request.user)
+        except UserProfile.DoesNotExist:
+            user_profile = UserProfile()
+            facebook_user = request.user.social_auth.get(provider='facebook')
+            facebook_id = facebook_user.uid
+            facebook_access_token = facebook_user.extra_data['access_token']
+            picture_url = 'https://graph.facebook.com/%s/picture/?width=500&height=500' % facebook_id
+            print picture_url
     return render(request, 'events/home.html', {})
