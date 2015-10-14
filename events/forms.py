6a376c5d5e6aee287e2b9fa9c4a33f22a8a502ab
seul zarraga <ingzarragaespinosa@gmail.com>
2015-10-14 14:05:25 -0500

    merge develop with my branch

---
+++ events/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from events.models import Event
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['name',
+                  'description',
+                  'requirements',
+                  'restrictions',
+                  'algo'
+                ]
---
+++ events/urls.py
@@ -0,0 +1,8 @@
+"""Events URLs."""
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^$', 'events.views.events_home', name='home'),
+    url(r'^crear_evento/$', 'events.views.create_event',
+        name='create_event'),
+]
