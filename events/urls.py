3f00da58644680b03e05a5e90cf55f09739f5efa
danielmcv <daneil625@outlook.com>
2015-10-15 20:41:39 -0500

    creado_events

--- events/urls.py
+++ events/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$', 'events.views.events_home', name='home'),
+    url(r'^events$', 'events.views.events', name='events'),
 ]
--- events/views.py
+++ events/views.py
@@ -24,3 +24,7 @@ def events_home(request):
             user_profile.facebook_url = 'https://www.facebook.com/app_scoped_user_id/%s/' % facebook_id
             user_profile.save()
     return render(request, 'events/home.html', {})
+
+
+def events(request):
+    return render(request, 'events/events.html', {})
