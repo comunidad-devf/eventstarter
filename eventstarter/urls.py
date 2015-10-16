e4b52083f8bf1f81eff0ec9051b05668043c176a
seul zarraga <ingzarragaespinosa@gmail.com>
2015-10-16 13:19:49 -0500

    imagen rota de eventos fondeados resuelto

--- eventstarter/urls.py
+++ eventstarter/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from eventstarter import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url('', include('events.urls', namespace='events')),
     url('', include('userprofiles.urls', namespace='userprofiles')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
